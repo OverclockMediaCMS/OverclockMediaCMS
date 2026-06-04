e93e740730eeddcb895c972d2335fe0a1fd9f850
Rory Coleman <rory.cosmic@yahoo.com>
2026-06-04 14:28:16 +1000

    before merge

--- src/utilities/postComponents.tsx
+++ src/utilities/postComponents.tsx
@@ -13,7 +13,6 @@ export const PostLimitedDisplay: FC<Post & {onClick: () => void}> = ({ Title, Us
   {
       hour: '2-digit', minute: '2-digit', year: "2-digit", month: "2-digit", day: '2-digit'
   });
-  const hasComments = Comments.length;
   return (
     <div className='postLimited'>
       <div onClick={onClick} >
