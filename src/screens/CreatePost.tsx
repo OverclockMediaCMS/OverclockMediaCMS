59e59de4b756e3a304f4cb5a09ed6e538bf889a0
HonT1 <tikhon.zavialov@studytafensw.edu.au>
2026-06-10 19:06:00 +1000

    create post hotfix

--- src/screens/CreatePost.tsx
+++ src/screens/CreatePost.tsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react";
-import { postToEndpoint } from "../utilities/helpers";
+import { postToEndpoint } from "../helpers";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../GlobalContext";
 import "../style/post.css"
